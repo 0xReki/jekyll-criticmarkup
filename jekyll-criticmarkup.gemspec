b74f8c60c207ab93d66fb46c7a918bba485fdb3c
Thai Chung <mail@0xreki.de>
2020-12-14 22:11:07 +0100

    Add newline support
    Also, nano and alike are just the worst. At least make it more like mcedit!

--- jekyll-criticmarkup.gemspec
+++ jekyll-criticmarkup.gemspec
@@ -2,7 +2,7 @@
 
 Gem::Specification.new do |spec|
   spec.name          = "jekyll-criticmarkup"
-  spec.version       = "0.0.1"
+  spec.version       = "0.0.2"
   spec.authors       = ["Thai Chung"]
   spec.email         = ["mail@0xReki.de"]
   spec.summary       = "CriticMarkup"
--- lib/jekyll-criticmarkup.rb
+++ lib/jekyll-criticmarkup.rb
@@ -1,12 +1,18 @@
 Jekyll::Hooks.register :documents, :pre_render do |document, payload|
   # Convert CriticMarkup to HTML
   content = document.content
-  
+
+  # Add Space to empty Markup
+  content = content.gsub(/\{\+\+\n\n(.*)\+\+\}/, "<ins>&nbsp;</ins>\n\n<ins>\\1</ins>")
   content = content.gsub(/\{\+\+(.*?)\+\+\}/m, '<ins>\1</ins>')
+
+  content = content.gsub(/\{--\n\n(.*)--\}/, "<del>&nbsp;</del>\n\n<del>\\1</del>")
   content = content.gsub(/\{--(.*?)--\}/m, '<del>\1</del>')
+
   content = content.gsub(/\{~~(.*?)->(.*?)~~\}/m, '<del>\1</del><ins>\2</ins>')
   content = content.gsub(/\{>>(.*?)<<\}/m, '<span class="critic-comment" style="right:0">\1</span>')
   content = content.gsub(/\{==(.*?)==\}/m, '<mark>\1</mark>')
+
   document.content = content
 end
 
@@ -17,10 +23,12 @@ Jekyll::Hooks.register :pages, :pre_render do |document, payload|
   content = document.content
 
   # Add Space to empty Markup
-#  content = content.gsub(/\{\+\+\n\n\+\+\}/, '<ins>&nbsp;</ins>')
-#  content = content.gsub(/\{--\n\n--\}/, '<del>&nbsp;</del>')
+  content = content.gsub(/\{\+\+\n\n(.*)\+\+\}/, "<ins>&nbsp;⏎</ins>\n\n<ins>\\1</ins>")
   content = content.gsub(/\{\+\+(.*?)\+\+\}/m, '<ins>\1</ins>')
+
+  content = content.gsub(/\{--\n\n(.*)--\}/, "<del>&nbsp;⏎</del>\n\n<del>\\1</del>")
   content = content.gsub(/\{--(.*?)--\}/m, '<del>\1</del>')
+
   content = content.gsub(/\{~~(.*?)->(.*?)~~\}/m, '<del>\1</del><ins>\2</ins>')
   content = content.gsub(/\{>>(.*?)<<\}/m, '<span class="critic-comment" style="right:0">\1</span>')
   content = content.gsub(/\{==(.*?)==\}/m, '<mark>\1</mark>')
